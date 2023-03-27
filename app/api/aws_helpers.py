ac7cd72877534f62682561cff58a8d7bc96e8645
Eli <elixfoster@gmail.com>
2023-03-27 02:26:55 -0700

    maybe working

--- app/api/aws_helpers.py
+++ app/api/aws_helpers.py
@@ -1,22 +1,24 @@
+
 import boto3
 import botocore
 import os
 import uuid
 
-BUCKET_NAME=os.environ.get("S3_BUCKET")
-S3_LOCATION= f"https://{BUCKET_NAME}.s3.amazonaws.com/"
-ALLOWED_EXTENSIONS={'mp4'}
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
+ALLOWED_EXTENSIONS= {"png", "jpg", "jpeg", "gif", "pdf"}
 
 s3 = boto3.client(
-    "s3",
-    aws_access_key_id=os.environ.get('S3_KEY'),
-    aws_secret_access_key=os.environ.get("S3_SECRET")
+   "s3",
+   aws_access_key_id=os.environ.get("S3_KEY"),
+   aws_secret_access_key=os.environ.get("S3_SECRET")
 )
 
 def get_unique_filename(filename):
-    ext = filename.split(".", 1)[1].lower()
+    ext = filename.rsplit(".", 1)[1].lower()
     unique_filename = uuid.uuid4().hex
     return f"{unique_filename}.{ext}"
+
 
 def upload_file_to_s3(file, acl="public-read"):
     try:
