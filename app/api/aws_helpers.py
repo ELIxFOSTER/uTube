b5b9da1cff184b6403b6c64305865158020645db
Eli <elixfoster@gmail.com>
2023-03-27 02:49:29 -0700

    file failed fix

--- app/api/aws_helpers.py
+++ app/api/aws_helpers.py
@@ -1,4 +1,3 @@
-
 import boto3
 import botocore
 import os
@@ -6,13 +5,14 @@ import uuid
 
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
-ALLOWED_EXTENSIONS= {"png", "jpg", "jpeg", "gif", "pdf"}
+ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 
 s3 = boto3.client(
    "s3",
    aws_access_key_id=os.environ.get("S3_KEY"),
    aws_secret_access_key=os.environ.get("S3_SECRET")
 )
+
 
 def get_unique_filename(filename):
     ext = filename.rsplit(".", 1)[1].lower()
