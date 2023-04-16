98893e1ec4332bd8a70f073a04e88891dddaefe6
Eli <elixfoster@gmail.com>
2023-04-16 03:04:54 -0400

    new seed

--- migrations/versions/8c0c839a2d7a_.py
+++ migrations/versions/8c0c839a2d7a_.py
@@ -1,20 +1,16 @@
 """empty message
 
-Revision ID: 3b71b64ca92b
-Revises:
-Create Date: 2023-04-15 19:58:11.610691
+Revision ID: 8c0c839a2d7a
+Revises: 
+Create Date: 2023-04-16 03:04:13.614628
 
 """
 from alembic import op
 import sqlalchemy as sa
 
-import os
-environment = os.getenv("FLASK_ENV")
-SCHEMA = os.environ.get("SCHEMA")
-
 
 # revision identifiers, used by Alembic.
-revision = '3b71b64ca92b'
+revision = '8c0c839a2d7a'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -34,16 +30,11 @@ def upgrade():
     sa.Column('updated_at', sa.DateTime(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
-
-    if environment == "production":
-        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
-
-
     op.create_table('videos',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('title', sa.String(), nullable=True),
+    sa.Column('title', sa.String(length=255), nullable=True),
     sa.Column('description', sa.String(length=255), nullable=True),
-    sa.Column('category', sa.String(), nullable=True),
+    sa.Column('category', sa.String(length=120), nullable=True),
     sa.Column('url', sa.String(), nullable=True),
     sa.Column('thumbnail', sa.String(), nullable=True),
     sa.Column('user_id', sa.Integer(), nullable=True),
@@ -52,11 +43,6 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-
-    if environment == "production":
-        op.execute(f"ALTER TABLE videos SET SCHEMA {SCHEMA};")
-
-
     op.create_table('comments',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('video_id', sa.Integer(), nullable=True),
@@ -68,9 +54,6 @@ def upgrade():
     sa.ForeignKeyConstraint(['video_id'], ['videos.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-
-    if environment == "production":
-        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
